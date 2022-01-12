7aaa73986dab8d4a2c579d22be790b24fc77f643
Simon Søhår <s173255@student.dtu.dk>
2022-01-12 14:01:34 +0100

    Create and Remove to DummyList

--- src/main/kotlin/screens/CreateAuctionScreen.kt
+++ src/main/kotlin/screens/CreateAuctionScreen.kt
@@ -1,24 +1,30 @@
 package screens
 
+import addItemToAuctions
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import auctionDummyData
+import auctions
 import factories.LoginItems
 import navcontroller.NavController
 
 @Composable
 fun CreateAuctionScreen(navController: NavController) {
-    var auctionTitle : String = ""
-    var auctionPrice : Int = 0
-    var auctionTime : Int = 0 // In seconds / ms
+    var auctionTitle = remember { mutableStateOf("") }
+    var auctionURL = remember { mutableStateOf("") }
+    var auctionPrice = remember { mutableStateOf(0) }
+    var auctionTime = remember { mutableStateOf(0) }
 
     Column(
         modifier = Modifier.fillMaxHeight(1f).fillMaxWidth(1f).padding(40.dp),
@@ -38,8 +44,8 @@ fun CreateAuctionScreen(navController: NavController) {
         }
         Row(modifier = Modifier.padding(5.dp)) {
             TextField(
-                value = auctionTitle,
-                onValueChange = { auctionTitle = it },
+                value = auctionTitle.value,
+                onValueChange = { auctionTitle.value = it },
                 label = { Text("Enter title") },
                 placeholder = { Text("Title") }
             )
@@ -47,8 +53,8 @@ fun CreateAuctionScreen(navController: NavController) {
         Row(modifier = Modifier.padding(5.dp)) {
             TextField(
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                value = auctionPrice.toString(),
-                onValueChange = { auctionPrice = it.toInt() },
+                value = auctionPrice.value.toString(),
+                onValueChange = { auctionPrice.value = it.toInt() },
                 label = { Text("Enter price in $") },
                 placeholder = { Text("Price") }
             )
@@ -56,14 +62,26 @@ fun CreateAuctionScreen(navController: NavController) {
         Row(modifier = Modifier.padding(5.dp)) {
             TextField(
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                value = auctionTime.toString(),
-                onValueChange = { auctionTime = it.toInt() },
+                value = auctionTime.value.toString(),
+                onValueChange = { auctionTime.value = it.toInt() },
                 label = { Text("Enter duration in ms") },
                 placeholder = { Text("Duration") }
             )
         }
         Row(modifier = Modifier.padding(5.dp)) {
-            Button(onClick = {navController.navigate(Screen.AuctionatorScreen.name)}) {
+            TextField(
+                value = auctionURL.value,
+                onValueChange = { auctionURL.value = it },
+                label = { Text("Enter Image URL") },
+                placeholder = { Text("Image URL") }
+            )
+        }
+        Row(modifier = Modifier.padding(5.dp)) {
+            Button(onClick = {
+                addItemToAuctions(auctionDummyData(auctions.size+1, auctionTitle.value, auctionPrice.value, auctionTime.value))
+                navController.navigate(Screen.AuctionatorScreen.name)
+            }
+            ) {
                 Text("Submit")
             }
         }
