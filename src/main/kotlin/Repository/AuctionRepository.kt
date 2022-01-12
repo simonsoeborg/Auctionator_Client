8477656a552d6d1740ec3dcc7c9dcd210ba8959c
Simon Søhår <s173255@student.dtu.dk>
2022-01-12 13:28:59 +0100

    Repo changes

--- src/main/kotlin/Repository/AuctionRepository.kt
+++ src/main/kotlin/Repository/AuctionRepository.kt
@@ -1,7 +1,7 @@
 package repository
 
 interface AuctionRepository {
-    suspend fun createAuction(userName : String, itemName : String, price : Int, endDate : String, endTime : String)
+    suspend fun createAuction(aId : Int, userName : String, itemName : String, price : Int, endDate : String, endTime : String, imgURL : String)
     suspend fun getAuction(variant : String) : Any
     suspend fun getHighestBid(variant : String) : Int
     suspend fun placeBid()
