d36bdc89b9d4d3b0be1e1175336972bea3bf1098
Simon Søhår <s173255@student.dtu.dk>
2022-01-12 12:56:50 +0100

    Form created

--- src/main/kotlin/Repository/AuctionRepository.kt
+++ src/main/kotlin/Repository/AuctionRepository.kt
@@ -1,7 +1,7 @@
 package repository
 
 interface AuctionRepository {
-    suspend fun createAuction(userName : String, itemName : String, price : Int, endDate : String, endTime : String, description : String)
+    suspend fun createAuction(userName : String, itemName : String, price : Int, endDate : String, endTime : String)
     suspend fun getAuction(variant : String) : Any
     suspend fun getHighestBid(variant : String) : Int
     suspend fun placeBid()
