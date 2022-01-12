6b1e91e9f793d8a2a40e871a58e046a2870cd2ad
Simon Søhår <s173255@student.dtu.dk>
2022-01-12 12:27:02 +0100

    CreateAuction + NavController

---
+++ src/main/kotlin/screens/CreateAuctionScreen.kt
@@ -0,0 +1,19 @@
+package screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import navcontroller.NavController
+
+@Composable
+fun CreateAuctionScreen(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxHeight(1f).fillMaxWidth(1f),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
+}
