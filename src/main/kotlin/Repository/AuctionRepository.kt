24ea629b44d148d4b3960ca2e6d285ca90eb88cf
Kasper Baun <kasperbaun@hotmail.com>
2022-01-13 16:25:14 +0100

    init

--- src/main/kotlin/Repository/AuctionRepository.kt
+++
@@ -1,8 +0,0 @@
-package repository
-
-interface AuctionRepository {
-    suspend fun createAuction(aId : Int, userName : String, itemName : String, price : Int, endDate : String, endTime : String, imgURL : String)
-    suspend fun getAuction(variant : String) : Any
-    suspend fun getHighestBid(variant : String) : Int
-    suspend fun placeBid()
-}
